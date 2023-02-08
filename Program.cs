2b15a322bd2d912e01c4d0c7e0f411d746f403d5
crisneames <crisneames@gmail.com>
2023-02-07 18:20:21 -0600

    Created menu

--- Program.cs
+++ Program.cs
@@ -1,34 +1,39 @@
 ﻿//Classes
 //Menu -- enums
 //District
-    //list of stores
-    //district manager
-    //inherit from employee (override retail sales)
-    //district name
-    //district sales -- totals store sales
+//list of stores
+//district manager
+//inherit from employee (override retail sales)
+//district name
+//district sales -- totals store sales
 //Store
-    //storeid (store number)
-    //gas yearly
-    //gas current quarter
-    //retail yearly
-    //retail current quarter
-    //store manager
-    //assistant manager
-    //list of associates
-    //store sales
+//storeid (store number)
+//gas yearly
+//gas current quarter
+//retail yearly
+//retail current quarter
+//store manager
+//assistant manager
+//list of associates
+//store sales
 //Employee -- base class for all types of employees.
-    //id
-    //name
-    //retail sales
+//id
+//name
+//retail sales
 //Store Manager -- inherit from employee
-    //title
-    //promote/demote other employees' titles
-    //fire employee (delete)
+//title
+//promote/demote other employees' titles
+//fire employee (delete)
 //Assistant Manager -- inherit from employee
-    //title
+//title
 //Associate -- inherit from employee
-    //title
-    //quite job
+//title
+//quite job
 //District Manager
-    //id
-    //name
+//id
+//name
+
+using Mammoth_Cave_Quick_Trippin_;
+
+Menu menu = new Menu();
+menu.Show();
