631c9ad9fee205c6e917db665f2c00067ae8906f
Mariana Mena <maranashokry@ahoo.com>
2023-02-07 18:21:01 -0600

    new branch

--- StoreManager.cs
+++ StoreManager.cs
@@ -1,5 +1,23 @@
 ﻿namespace Mammoth_Cave_Quick_Trippin_;
 
-public class StoreManager
+public class StoreManager : Employee, IPromote_Fire_Hire
 {
+    //promote/demote other employees' titles
+    //fire employee (delete)
+    public DateTime HiredDate { get; set; } = DateTime.Now.Date;
+    public DateTime FiredDate { get; set; }= DateTime.Now.Date;
+    public string Promote { get; set; }
+
+    public void Hire(DateTime hiredDate)
+    {
+        Console.WriteLine("Hire");
+    }
+    public void Fire(Employee employee)
+    {
+        employee.Employees.Clear();
+    }
+    public void promotion(Employee employee)
+    {
+        
+    }
 }
